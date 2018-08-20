b781c5d2031629569791cf0f1ebba356e0aeb2f4
Anderson Gimino <andersongimino@gmail.com>
2018-08-20 14:46:03 -0300

    Novo app para ajustar versões antes de iniciar desenvolvimento de pocs.

--- my-poc-app/e2e/src/app.e2e-spec.ts
+++ my-poc-app/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to my-app-angular!');
+    expect(page.getParagraphText()).toEqual('Welcome to my-poc-app!');
   });
 });
--- my-poc-app/src/app/app.module.ts
+++ my-poc-app/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
